00622eb83b3a4ab0946a52486189da0375138093
noka <noka.hieunt@gmail.com>
2021-09-14 09:57:07 +0700

    first commit

---
+++ src/main/java/com/nokavietnam/jdbc/JdbcApplication.java
@@ -0,0 +1,13 @@
+package com.nokavietnam.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcApplication.class, args);
+    }
+
+}
